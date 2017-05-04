3abe37f7aacfc135a2e0657888b54b853c04e154
erickao08 <erickao08@hotmail.com>
2017-05-04 01:26:16 -0500

    SSE

--- Wine prediction.R
+++ Wine prediction.R
@@ -6,3 +6,14 @@ summary(wine)
 ##one variable linear regression model  lm(dependent ~independent, data= dataset)
 model1<-lm(Price ~AGST,data=wine)
 summary(model1)
+
+model1$residuals
+
+SSE<- sum(model1$residuals^2)
+
+model2<-lm(Price ~AGST+HarvestRain, data=wine)
+model2
+SSE2<-sum(model2$residuals^2)
+
+model3<-lm(Price ~AGST+HarvestRain+WinterRain+Age+FrancePop, data=wine)
+SSE3 <- sum(model3$residuals^2)
