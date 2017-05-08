445794a739ca736cba4fb6446917e0f249293b77
erickao08 <erickao08@hotmail.com>
2017-05-08 01:16:33 -0500

    Prediction

--- Wine prediction.R
+++ Wine prediction.R
@@ -17,3 +17,16 @@ SSE2<-sum(model2$residuals^2)
 
 model3<-lm(Price ~AGST+HarvestRain+WinterRain+Age+FrancePop, data=wine)
 SSE3 <- sum(model3$residuals^2)
+
+model_all<-lm(Price ~.-FrancePop,data=wine)
+model4<-lm(Price~.-FrancePop-Age,data=wine)
+
+wine_test<-read.csv('wine_test.csv')
+str(wine_test)
+
+predict_wine_test<-predict(model4,newdata=wine_test)
+
+
+SSE_pre<-sum((wine_test$Price-predict_wine_test)^2)
+SST_pre<-sum((wine_test$Price-mean(wine$Price))^2)
+R_sqare<-1-(SSE_pre/SST_pre)
